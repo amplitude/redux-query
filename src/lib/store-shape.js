d2e3ddcf718eea39567002770bc27712c4c6bc6c
Ryan Ashcraft <ryan@amplitude.com>
2017-02-12 14:55:26 -0800

    Add a few more lint rules

--- src/lib/store-shape.js
+++ src/lib/store-shape.js
@@ -3,5 +3,5 @@ import { PropTypes } from 'react';
 export default PropTypes.shape({
     subscribe: PropTypes.func.isRequired,
     dispatch: PropTypes.func.isRequired,
-    getState: PropTypes.func.isRequired
+    getState: PropTypes.func.isRequired,
 });

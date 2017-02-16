7c9fa9aa507db91cab872c17681a0a7b7496c5f2
Nicholas van de Walle <nicholasvandewalle@waypointbuilding.com>
2017-02-16 10:13:01 -0800

    Install lodash set components and prototype multiple configs being returned as an array

--- src/components/connect-request.js
+++ src/components/connect-request.js
@@ -1,4 +1,6 @@
 import partial from 'lodash.partial';
+import difference from 'lodash.difference'
+import intersection from 'lodash.intersection'
 import React from 'react';
 import shallowEqual from 'react-pure-render/shallowEqual';
 
@@ -27,18 +29,33 @@ const connectRequest = (mapPropsToConfig, options = {}) => (WrappedComponent) =>
         }
 
         componentDidMount() {
-            this.requestAsync(this.props, false, true);
+            const configs = mapPropsToConfig(configs);
+            if (Array.isArray(configs)) {
+                this.requestAsync(configs, false, true);
+            } else {
+                this.requestAsync(this.props, false, true);
+            }
         }
 
         componentDidUpdate(prevProps) {
-            const prevConfig = mapPropsToConfig(prevProps);
-            const config = mapPropsToConfig(this.props);
-            const prevQueryKey = prevConfig && getQueryKey(prevConfig.url, prevConfig.body);
-            const queryKey = config && getQueryKey(config.url, config.body);
+            let prevConfigs = mapPropsToConfig(prevProps);
+            let configs = mapPropsToConfig(this.props);
+            if (!Array.isArray(prevConfigs)) {
+                prevConfigs = [prevConfigs];
+            }
+            if (!Array.isArray(configs)) {
+                configs = [configs];
+            }
 
-            if (prevQueryKey !== queryKey) {
-                this.cancelPendingRequests();
-                this.requestAsync(this.props, false, true);
+            const diffs = this.diffConfigs(prevConfigs, configs);
+            const toCancel = diffs[0];
+            const toRequest = diffs[1];
+
+            if (toCancel.length) {
+                this.cancelPendingRequests(toCancel);
+            }
+            if (toRequest.length) {
+                this.requestAsync(toRequest);
             }
         }
 
@@ -50,39 +67,77 @@ const connectRequest = (mapPropsToConfig, options = {}) => (WrappedComponent) =>
             return this._wrappedInstance;
         }
 
-        cancelPendingRequests() {
-            const { dispatch } = this.context.store;
+        // returns [[ "keys", "to", "cancel"], [{ config objects to request }]]
+        _diffConfigs(prevConfigs, configs) {
+            const unpack = (c) => {
+                return getQueryKey(c.url, c.body);
+            };
+            const prevQueryKeys = prevConfigs.map(unpack);
+            const queryKeys = configs.map(unpack);
+            const i = intersection(prevQueryKeys, queryKeys);
+            const cancelKeys = difference(prevQueryKeys, i);
+            const requestKeys = difference(queryKeys, i);
 
-            for (const queryKey in this._pendingRequests) {
-                if (this._pendingRequests.hasOwnProperty(queryKey)) {
-                    dispatch(cancelQuery(queryKey));
+            const toRequest = prevConfigs.filter((c) => {
+                const k = getQueryKey(c.url, c.body);
+                return requestKeys.includes(k);
+            });
+
+            return [cancelKeys, toRequest];
+        }
+
+        cancelPendingRequests(toCancel) {
+            const { dispatch } = this.context.store;
+            if (toCancel && Array.isArray(toCancel)) {
+                // Only cancel specific keys, supports multiple configs
+                toCancel.forEach((queryKey) => {
+                    if (this._pendingRequests.hasOwnProperty(queryKey)) {
+                        dispatch(cancelQuery(queryKey));
+                    }
+                });
+            } else {
+                for (const queryKey in this._pendingRequests) {
+                    if (this._pendingRequests.hasOwnProperty(queryKey)) {
+                        dispatch(cancelQuery(queryKey));
+                    }
                 }
             }
         }
 
-        requestAsync(props, force = false, retry = false) {
+        requestAsync(propsOrConfigs, force = false, retry = false) {
+            if (Array.isArray(propsOrConfigs)) {
+                // propsToConfig mapping has happened already
+                propsOrConfigs.forEach((c) => {
+                    this._makeRequest(c, force, retry);
+                });
+            } else {
+                const config = mapPropsToConfig(propsOrConfigs);
+
+                if (config) {
+                    this._makeRequest(config, force, retry);
+                }
+            }
+        }
+
+        _makeRequest(config, force, retry) {
             const { dispatch } = this.context.store;
-            const config = mapPropsToConfig(props);
+            const { url, body } = config;
 
-            if (config) {
-                const { url, body } = config;
+            if (url) {
+                const requestPromise = dispatch(requestAsync({
+                    force,
+                    retry,
+                    ...config,
+                }));
 
-                if (url) {
-                    const requestPromise = dispatch(requestAsync({
-                        force,
-                        retry,
-                        ...config,
-                    }));
+                if (requestPromise) {
+                    // Record pending request since a promise was returned
+                    const queryKey = getQueryKey(url, body);
+                    this._pendingRequests[queryKey] = null;
 
-                    if (requestPromise) {
-                        // Record pending request since a promise was returned
-                        const queryKey = getQueryKey(url, body);
-                        this._pendingRequests[queryKey] = null;
-
-                        requestPromise.then(() => {
-                            delete this._pendingRequests[queryKey];
-                        });
-                    }
+                    requestPromise.then(() => {
+                        delete this._pendingRequests[queryKey];
+                    });
                 }
             }
         }

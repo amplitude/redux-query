629dcc5749a4fe5680cf30235dba4ee5a63adb5a
Ryan Ashcraft <ryan@ryanashcraft.me>
2017-03-18 16:16:28 -0700

    Fix advanced invalid imports

--- src/advanced.js
+++ src/advanced.js
@@ -1,10 +1,10 @@
 import * as actions from './actions';
 import * as actionTypes from './constants/action-types';
 import * as httpMethods from './constants/http-methods';
-import * as querySelectors from './selectors/query-selectors';
+import * as querySelectors from './selectors/query';
 
 export { default as connectRequest } from './components/connect-request';
-export { default as getQueryKey } from './lib/get-query-key';
+export { getQueryKey, reconcileQueryKey } from './lib/query-key';
 export { default as queriesReducer } from './reducers/queries';
 export { default as entitiesReducer } from './reducers/entities';
 export { default as queryMiddlewareAdvanced } from './middleware/query-advanced';

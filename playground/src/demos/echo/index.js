dc0387695420152ca10f33f1f3c62cc9cdf1a17f
Ryan Ashcraft <ryan@ryanashcraft.me>
2017-03-28 17:01:14 -0700

    Echo demo

---
+++ playground/src/demos/echo/index.js
@@ -0,0 +1,7 @@
+import clientCode from './client.txt';
+import serverCode from './server.txt';
+
+export default {
+    clientCode,
+    serverCode,
+};
--- playground/src/index.js
+++ playground/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Playground from './components/Playground';
-import demo from './demos/hello-world';
+import demo from './demos/echo';
 import './index.css';
 
 ReactDOM.render(

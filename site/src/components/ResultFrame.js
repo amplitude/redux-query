8272011c434b3e2e2e8be639a315d7443636a9e9
Ryan Ashcraft <ryan@amplitude.com>
2017-04-25 12:31:21 -0700

    Allow running site with locally-compiled redux-query

--- site/src/components/ResultFrame.js
+++ site/src/components/ResultFrame.js
@@ -62,6 +62,7 @@ class ResultFrame extends Component {
       'https://unpkg.com/redux-saga@0.14/dist/redux-saga.min.js',
       'https://unpkg.com/qs@6.0.4/dist/qs.js',
       'https://unpkg.com/redux-query@1.5.0-alpha.1/dist/umd/redux-query.js',
+      '/vendor/redux-query.js',
     ];
 
     const contentWindow = this._iframeRef.contentWindow;

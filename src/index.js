47d19214f71a3eabe141180c57a36fbca6646547
Ryan Ashcraft <ryan@amplitude.com>
2017-05-02 09:48:55 -0700

    Fix entry points

--- src/index.js
+++ src/index.js
@@ -12,5 +12,5 @@ export { default as entitiesReducer } from './reducers/entities';
 export { default as errorsReducer } from './reducers/errors';
 export { default as queryMiddleware } from './middleware/query';
 export { default as queryMiddlewareAdvanced } from './middleware/query-advanced';
-export { cancelQuery, mutateAsync, requestAsync, removeEntities, removeEntity } from './actions';
+export { cancelQuery, mutateAsync, requestAsync, updateEntities } from './actions';
 export { actions, actionTypes, errorSelectors, httpMethods, networkInterfaces, querySelectors };

888680cf444bc5abdc200da431eae84f610b80c7
Ryan Ashcraft <ryan@amplitude.com>
2017-05-02 15:32:28 -0700

    Allow undefined/null queryConfig arguments to selectors

--- src/selectors/query.js
+++ src/selectors/query.js
@@ -3,31 +3,41 @@ import get from 'lodash.get';
 import { getQueryKey } from '../lib/query-key';
 
 export const isFinished = (queriesState, queryConfig) => {
-    const queryKey = getQueryKey(queryConfig);
+    if (queryConfig) {
+        const queryKey = getQueryKey(queryConfig);
 
-    return get(queriesState, [queryKey, 'isFinished']);
+        return get(queriesState, [queryKey, 'isFinished']);
+    }
 };
 
 export const isPending = (queriesState, queryConfig) => {
-    const queryKey = getQueryKey(queryConfig);
+    if (queryConfig) {
+        const queryKey = getQueryKey(queryConfig);
 
-    return get(queriesState, [queryKey, 'isPending']);
+        return get(queriesState, [queryKey, 'isPending']);
+    }
 };
 
 export const status = (queriesState, queryConfig) => {
-    const queryKey = getQueryKey(queryConfig);
+    if (queryConfig) {
+        const queryKey = getQueryKey(queryConfig);
 
-    return get(queriesState, [queryKey, 'status']);
+        return get(queriesState, [queryKey, 'status']);
+    }
 };
 
 export const lastUpdated = (queriesState, queryConfig) => {
-    const queryKey = getQueryKey(queryConfig);
+    if (queryConfig) {
+        const queryKey = getQueryKey(queryConfig);
 
-    return get(queriesState, [queryKey, 'lastUpdated']);
+        return get(queriesState, [queryKey, 'lastUpdated']);
+    }
 };
 
 export const queryCount = (queriesState, queryConfig) => {
-    const queryKey = getQueryKey(queryConfig);
+    if (queryConfig) {
+        const queryKey = getQueryKey(queryConfig);
 
-    return get(queriesState, [queryKey, 'queryCount']);
+        return get(queriesState, [queryKey, 'queryCount']);
+    }
 };

fcfa16d4865af2e030e893311ac23865ebad4d51
Ryan Ashcraft <ryan@amplitude.com>
2019-06-05 12:20:37 -0700

    Fix build

---
+++ packages/redux-query-react/src/index.js
@@ -0,0 +1,7 @@
+import connectRequest from './components/connect-request';
+import useRequest from './hooks/use-request';
+import useMutation from './hooks/use-mutation';
+
+export { connectRequest };
+export { useRequest };
+export { useMutation };

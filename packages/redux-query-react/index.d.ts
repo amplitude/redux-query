a3b199235aa7b9efca5d4be50b31ca89cd87e171
ricfang <ricfang@gmail.com>
2020-10-15 17:24:33 -0700

    update hook Typescript types (#204)
    * update hook Typescript types
    
    * add back ActionPromise export

--- packages/redux-query-react/index.d.ts
+++ packages/redux-query-react/index.d.ts
@@ -22,7 +22,10 @@ declare module 'redux-query-react' {
   }
   export type ForceRequestCallback<TEntities = Entities> = () => Promise<
     ActionPromiseValue<TEntities>
-  >;
+  > | void;
+
+  export type ForceRequestsCallback = () => void;
+
   export interface WithForceRequest<TEntities = Entities> {
     forceRequest: ForceRequestCallback<TEntities>;
   }
@@ -45,9 +48,7 @@ declare module 'redux-query-react' {
   }
   export type ReduxQueryProvider = React.ComponentType<ProviderProps>;
 
-  export type ActionPromise<TEntities = Entities> =
-    | Promise<ActionPromiseValue<TEntities>>
-    | undefined;
+  export type ActionPromise<TEntities = Entities> = Promise<ActionPromiseValue<TEntities>>;
   type RunMutation<TEntities = Entities, TArgs = never> = (
     ...args: TArgs
   ) => ActionPromise<TEntities>;
@@ -58,7 +59,7 @@ declare module 'redux-query-react' {
 
   export type UseRequestsHook = <TEntities = Entities>(
     queryConfigs: Array<QueryConfig<TEntities> | null | undefined> | null | undefined,
-  ) => [QueryState, ForceRequestCallback<TEntities>];
+  ) => [QueryState, ForceRequestsCallback];
 
   export type UseMutationHook = <TEntities = Entities, TArgs = never>(
     createQueryConfig: QueryConfigFactory<TEntities, TArgs>,
